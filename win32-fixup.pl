1a7cff0f438adac32ac4fe98ab2d475a64949113
John Emmas <johne53@tiscali.co.uk>
2013-05-01 15:08:37 +0100

    JOHNE - Finalize for pushing to Github

---
+++ win32-fixup.pl
@@ -0,0 +1,57 @@
+#! e:/program files/perl/bin/perl.exe
+#  version info can be found in 'NEWS'
+
+require "../local-paths.lib";
+
+$api_version = "0.9";
+$pango_api_version = "1.0";
+$pango_current_minus_age = 0;
+$harfbuzz_version = "0.9.16";
+$major = 0;
+$minor = 9;
+$micro = 16;
+$binary_age = 90;
+$interface_age = 0;
+$current_minus_age = 16;
+
+sub process_file
+{
+        my $outfilename = shift;
+	my $infilename = $outfilename . ".in";
+	
+	open (INPUT, "< $infilename") || exit 1;
+	open (OUTPUT, "> $outfilename") || exit 1;
+	
+	while (<INPUT>) {
+	    s/\@PANGO_API_VERSION@/$pango_api_version/g;
+	    s/\@HARFBUZZ_API_VERSION@/$api_version/g;
+	    s/\@HB_VERSION@/$harfbuzz_version/g;
+	    s/\@HB_VERSION_MAJOR\@/$major/g;
+	    s/\@HB_VERSION_MINOR\@/$minor/g;
+	    s/\@HB_VERSION_MICRO\@/$micro/g;
+	    s/\@HB_INTERFACE_AGE\@/$interface_age/g;
+	    s/\@HB_BINARY_AGE\@/$binary_age/g;
+	    s/\@PANGO_CURRENT_MINUS_AGE@/$pango_current_minus_age/g;
+	    s/\@LT_CURRENT_MINUS_AGE@/$current_minus_age/g;
+	    s/\@PERL_PATH@/$perl_path/g;
+	    s/\@GlibBuildRootFolder@/$glib_build_root_folder/g;
+	    s/\@PangoBuildProjectFolder@/$pango_build_project_folder/g;
+	    s/\@GenericIncludeFolder@/$generic_include_folder/g;
+	    s/\@GenericLibraryFolder@/$generic_library_folder/g;
+	    s/\@GenericWin32LibraryFolder@/$generic_win32_library_folder/g;
+	    s/\@GenericWin32BinaryFolder@/$generic_win32_binary_folder/g;
+	    s/\@Debug32TestSuiteFolder@/$debug32_testsuite_folder/g;
+	    s/\@Release32TestSuiteFolder@/$release32_testsuite_folder/g;
+	    s/\@Debug32TargetFolder@/$debug32_target_folder/g;
+	    s/\@Release32TargetFolder@/$release32_target_folder/g;
+	    s/\@TargetSxSFolder@/$target_sxs_folder/g;
+	    print OUTPUT;
+	}
+}
+
+process_file ("src/hb-version.h");
+
+my $command=join(' ',@ARGV);
+if ($command eq -buildall) {
+	process_file ("msvc/harfbuzz.vsprops");
+}
