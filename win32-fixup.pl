f52ce2a1521d4a30bcaa9f767fd2f60e6b6dc846
John Emmas <johne53@tiscali.co.uk>
2013-06-14 12:23:09 +0100

    JOHNE - Update our project after merging from upstream

--- win32-fixup.pl
+++ win32-fixup.pl
@@ -6,13 +6,13 @@ require "../local-paths.lib";
 $api_version = "0.9";
 $pango_api_version = "1.0";
 $pango_current_minus_age = 0;
-$harfbuzz_version = "0.9.16";
+$harfbuzz_version = "0.9.18";
 $major = 0;
 $minor = 9;
-$micro = 16;
-$binary_age = 90;
+$micro = 18;
+$binary_age = 918;
 $interface_age = 0;
-$current_minus_age = 16;
+$current_minus_age = 18;
 
 sub process_file
 {
