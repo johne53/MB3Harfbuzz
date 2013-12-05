6903ae3e224dedb000a731243598bce0e48a6704
John Emmas <johne53@tiscali.co.uk>
2013-12-05 19:14:51 +0000

    JOHNE - Bump the version number to 0.9.25

--- win32-fixup.pl
+++ win32-fixup.pl
@@ -5,12 +5,12 @@ require "../local-paths.lib";
 
 $api_version = "0.9";
 $pango_current_minus_age = 0;
-$harfbuzz_version = "0.9.24";
+$harfbuzz_version = "0.9.25";
 $major = 0;
 $minor = 9;
-$micro = 24;
-$binary_age = 924;
-$interface_age = 24;
+$micro = 25;
+$binary_age = 925;
+$interface_age = 25;
 $current_minus_age = 0;
 $exec_prefix = "lib";
 
